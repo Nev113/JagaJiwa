938936da08a6d5ea03492f85a2161775bae5463c
Nevo Bias Restu Sangkane <61056577+Nev113@users.noreply.github.com>
2025-06-08 18:50:08 +0700

    fix: eslint

--- app/api/route.js
+++ app/api/route.js
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return NextResponse.json({ message: "Hello, world!" });
-  } catch (error) {
-    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
-  }
+  return NextResponse.json({
+    message: "Hello, World!",
+    timestamp: new Date().toISOString(),
+  });
 }
