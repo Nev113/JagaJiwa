4cd22b61f2d9199982e5e490f26e1edb35a6abe0
Nevo Bias Restu Sangkane <61056577+Nev113@users.noreply.github.com>
2025-06-08 21:13:19 +0700

    Add a config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  distDir: "build",
   output: "standalone",
 };
 
